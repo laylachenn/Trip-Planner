be4da4c1a3f6561722479e8c1d735c08ee761a69
laylachen <laylachenn@gmail.com>
2023-10-13 14:01:51 -0700

    Added TripTest and implemented it

--- src/test/model/TripTest.java
+++ src/test/model/TripTest.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 //inspired by Teller application in the AccountTest class
 //source asList: https://www.geeksforgeeks.org/arrays-aslist-method-in-java-with-examples/
@@ -39,6 +38,48 @@ class TripTest {
         assertEquals(5, testTrip.getTripLength());
         assertEquals(expectedFlights, testTrip.getFlights());
         assertEquals(expectedHotels, testTrip.getHotels());
+        assertEquals(expectedDestinations, testTrip.getDestinations());
+    }
+
+    @Test
+    public void setTripLength() {
+        testTrip.setTripLength(8);
+        assertEquals(8,testTrip.getTripLength());
+    }
+
+    @Test
+    public void testAddFlights() {
+        testTrip = new Trip(new ArrayList<>(), 0, new ArrayList<>(), new ArrayList<>());
+        testTrip.addFlight("YVR-JFK 10/25/23 @11am");
+        testTrip.addFlight("JFK-YVR 10/30/23 @2pm");
+        List<String> expectedFlights = new ArrayList<>();
+        expectedFlights.add("YVR-JFK 10/25/23 @11am");
+        expectedFlights.add("JFK-YVR 10/30/23 @2pm");
+
+        assertEquals(expectedFlights, testTrip.getFlights());
+    }
+
+    @Test
+    public void testAddHotels() {
+        testTrip = new Trip(new ArrayList<>(), 0, new ArrayList<>(), new ArrayList<>());
+        testTrip.addHotel("Moxy NYC Times Square");
+        testTrip.addHotel("Motto by Hilton New York City Chelsea");
+        List<String> expectedHotels = new ArrayList<>();
+        expectedHotels.add("Moxy NYC Times Square");
+        expectedHotels.add("Motto by Hilton New York City Chelsea");
+
+        assertEquals(expectedHotels, testTrip.getHotels());
+    }
+
+    @Test
+    public void testAddDestinations() {
+        testTrip = new Trip(new ArrayList<>(), 0, new ArrayList<>(), new ArrayList<>());
+        testTrip.addDestination("Bronxville");
+        testTrip.addDestination("Yorkville");
+        List<String> expectedDestinations = new ArrayList<>();
+        expectedDestinations.add("Bronxville");
+        expectedDestinations.add("Yorkville");
+
         assertEquals(expectedDestinations, testTrip.getDestinations());
     }
 }
